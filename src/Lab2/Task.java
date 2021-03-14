20461580ee2c9ea119b449c8d7468bf55b5c44f2
MatLew497 <matlew017@utp.edu.pl>
2021-03-14 17:05:23 +0100

    Cw01

---
+++ src/Lab2/Task.java
@@ -0,0 +1,9 @@
+package Lab2;
+
+public class Task {
+    private String taskName;
+    private String taskDescription;
+    private taskCategory taskCategory;
+    private String taskDateTimeStart;
+    private String taskDateTimeStop;
+}
